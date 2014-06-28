9cbb0d8393e963a8913caa280e18c9d5ab6d8a7c
th86 <th8623@gmail.com>
2014-06-28 10:28:20 -0400

    equalci function
    fix the header file for equalci function

--- src/ci.h
+++ src/ci.h
@@ -4,3 +4,6 @@
 
 double 	exactci_c(const double* , const double*, R_len_t);
 SEXP 	exactciR2C(SEXP, SEXP);
+
+double 	equalci_c(const double* , const double*, R_len_t);
+SEXP 	equalciR2C(SEXP, SEXP);
