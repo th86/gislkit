73c1d51900884f3d4a89103bbba5c241744e6857
th86 <th8623@gmail.com>
2014-08-06 12:22:19 -0400

    Cross validated machine learning algorithms
    Add the interfaces to SVM, Cox, GBM cross validated feature selection
    functions.

---
+++ R/cv.r
@@ -0,0 +1,79 @@
+#cross validated feature selection of the machine learning algorithms
+require("e1071")
+require("survival")
+library("gbm")
+#require("glmnet")
+
+#cv.logistic
+#cv.logistic<-cv.glmnet(X, resp, nfolds=10, family="binomial" ,type.measure="auc")
+#logistic_model<-glm(resp~.,family=binomial("logit"),data=data.frame(X))
+
+cv.svm <- function(data, iterations=100, useFULL=FALSE, fold=0.75, kernel = "linear",  cost=1) {
+	ci_list=rep(0,iterations)
+	for(iter in 1:iterations){
+
+		if(useFULL==TRUE){
+	    	trainSet=data[sample(rownames(data), floor(nrow(data)*fold)),]
+	    	testSet=data[setdiff(rownames(data),rownames(trainSet)),]
+	 	}else{
+	 		trainSet=data
+	 		testSet=data	
+	 	}
+
+	svm_model<-svm(   resp~., data=trainSet , kernel=kernel, type="C",scale=TRUE , cost=cost ,cross=10, probability=TRUE) #gamma 0.058
+    pred<-predict(svm_model, testSet)
+    ci_list[iter]<-exactci(pred, Surv(1-testSet[,"resp"], testSet[,"resp"]))
+
+    }
+    return(mean(ci_list,na.rm=TRUE) )
+}
+
+
+cv.cox <- function(data, iterations=100, useFULL=FALSE, fold=0.75) {
+	ci_list=rep(0,iterations)
+	for(iter in 1:iterations){
+
+		if(useFULL==TRUE){
+	    	trainSet=data[sample(rownames(data), floor(nrow(data)*fold)),]
+	    	testSet=data[setdiff(rownames(data),rownames(trainSet)),]
+	 	}else{
+	 		trainSet=data
+	 		testSet=data	
+	 	}
+
+	    cox_model<-tryCatch({
+	     				coxph(Surv(time, cens) ~., data = trainSet, control = coxph.control(iter.max = 100))
+	     				},warning=function(w){
+	     				},error=function(e){
+	     					print(e)
+	     				},finally= {
+	     				})
+	    if(is.null(cox_model)==FALSE){
+		    cox_pred<-predict(cox_model, testSet)
+	    	pred=cox_pred
+	     	ci_list[iter]<-exactci(pred, Surv( testSet[,"time"],testSet[,"cens"]) )
+	    }
+	}
+    return(mean(ci_list,na.rm=TRUE) )
+}
+
+cv.gbm <- function(data, iterations=10, useFULL=FALSE, fold=0.75, cv.folds=3,shrinkage=0.05) {
+	ci_list=rep(0,iterations)
+	for(iter in 1:iterations){
+
+		if(useFULL==TRUE){
+	    	trainSet=data[sample(rownames(data), floor(nrow(data)*fold)),]
+	    	testSet=data[setdiff(rownames(data),rownames(trainSet)),]
+	 	}else{
+	 		trainSet=data
+	 		testSet=data	
+	 	}
+
+	 gbm_model <- gbm(Surv(time, cens) ~., data = trainSet, distribution="coxph" ,cv.folds=cv.folds, shrinkage=shrinkage, verbose = FALSE)
+  
+     gbm_pred<-predict(gbm_model, testSet)
+     pred=gbm_pred
+     ci_list[iter]<-exactci(pred, Surv( testSet[,"time"],testSet[,"cens"]) )
+    }
+    return(mean(ci_list,na.rm=TRUE) )
+}
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 gislkit
 ====
 
-install.packages("gislkit", repo=NULL, type="source")
+Download this package from https://github.com/th86/gislkit
+uncompress this package gislkit-master.zip as a directory
+Open R and install this package* as the following
+install.packages("gislkit-master", repo=NULL, type="source")
+
+
+*Windows users may need to install Rtools http://cran.r-project.org/bin/windows/Rtools/
