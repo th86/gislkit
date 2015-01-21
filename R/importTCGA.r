5297fda094134efbc2d5459bc672c170b7b43280
th86 <th8623@gmail.com>
2015-01-21 13:58:59 -0500

    Import function for TCGA RPKM files
    Import function for TCGA RPKM files.

--- R/importTCGA.r
+++ R/importTCGA.r
@@ -17,6 +17,27 @@ importTCGA<-function(path_customized="./", pattern_customized=".rsem.genes.norma
     return(ge)
 }
 
+#Import data from the TCGA compressed package normalized in the RPKM format as a matrix
+importRPKM<-function(path_customized="./", pattern_customized=".gene.quantification.txt", verbose=TRUE){
+    fileList=dir(path=path_customized,pattern=pattern_customized)
+    example<-read.table(paste(path_customized, fileList[1], sep=""), header=TRUE, sep="\t"   )
+
+    ge<-matrix(0,nrow(example),length(fileList))
+    colnames(ge)<-fileList
+    rownames(ge)<-as.character(example[,1])
+    rm(example)
+    for( fileName in fileList){
+        e<-read.table(paste(path_customized, fileName, sep=""), header=TRUE, sep="\t"   )
+        ge[,fileName]<-as.numeric(e[,4]) #RPKM
+        if(verbose==TRUE)
+            cat(which(fileList==fileName),"\n")
+    }
+    rm(e)
+    colnames(ge)<-substr(colnames(ge),1,28)
+
+    return(ge)
+}
+
 #Converts UUIDs in a TCGA data matrix to TCGA barcodes. 
 convertUUID2barcode<-function(ge,path ="./"){
     map<-read.table(paste(path, "FILE_SAMPLE_MAP.txt",sep=""),sep="\t",header=T)
@@ -25,3 +46,4 @@ convertUUID2barcode<-function(ge,path ="./"){
     colnames(ge)<-cn
     return(ge)
 }
+
