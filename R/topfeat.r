dde2640f81340fcbd7c56cf225d52a83ce341300
th86 <th8623@gmail.com>
2015-03-16 12:36:57 -0400

    topfeat
    distinguish the NMF object from ordinary matrices

--- R/topfeat.r
+++ R/topfeat.r
@@ -1,5 +1,11 @@
 #Print top features in each column
 topfeat<-function(obj,list_length=10){
-	w<-basis(obj)
+	if(class(obj)[1]=="NMFfit"){
+		library("NMF")
+		w<-basis(obj)
+	}else{
+		w<-obj
+	}
+
 	for(i in 1:ncol(w) ) cat(i,"\n",names(sort(w[,i],decreasing=T))[1:list_length],"\n",round(sort(w[,i],decreasing=T)[1:list_length],4),"\n")
 }
