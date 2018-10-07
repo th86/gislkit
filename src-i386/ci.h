782602543218b351a96796b95b4d62ca4d535afe
Tai-Hsien OuYang <th8623@gmail.com>
2018-10-07 09:01:38 -0400

    Removed object files

--- src-i386/ci.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <R.h>
-#include <Rdefines.h>
-
-double 	exactci_c(const double* , const double*, R_len_t);
-SEXP 	exactciR2C(SEXP, SEXP);
-
-double 	equalci_c(const double* , const double*, R_len_t);
-SEXP 	equalciR2C(SEXP, SEXP);
